eb2ed67299befbe491f30255e20571f750e537d4
Admin <admin@Wanjings-Mac.local>
2020-05-08 00:55:36 -0700

    complete 4 UIs

---
+++ gradual/gradual/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  gradual
+//
+//  Created by Admin on 5/7/20.
+//  Copyright © 2020 everydev. All rights reserved.
+//
+
+import UIKit
+import Stripe
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Stripe.setDefaultPublishableKey("")
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ gradual/gradual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gradual
+//
+//  Created by Admin on 5/7/20.
+//  Copyright © 2020 everydev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
